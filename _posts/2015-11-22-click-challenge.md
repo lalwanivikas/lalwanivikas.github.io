f14ee89dd070f3a9748715d5c4b86019a3249129
Vikas Lalwani <lalwani.vikas20@gmail.com>
2015-11-23 11:26:40 +0530

    day 22 project - click challenge

---
+++ _posts/2015-11-22-click-challenge.md
@@ -0,0 +1,12 @@
+---
+layout: blog
+title: Click Challenge
+day: 22
+---
+
+As I mentioned yesterday for next couple of days I am going to make very tiny apps as I am little occupied in other urgent things.
+
+
+With that said, here is today's tiny app: [http://vikaslalwani.com/projects/click-challenge/](http://vikaslalwani.com/projects/click-challenge/).
+
+Keep clicking on the right-hand side box for 30 seconds to know your click speed!
---
+++ projects/click-challenge/js/click.js
@@ -0,0 +1,45 @@
+var container = document.getElementsByClassName('container');
+var time = document.getElementById('time');
+var counter = document.getElementById('counter');
+var clickCount = document.getElementById('clickCount');
+var startMessage = document.getElementById('startMessage');
+var result = document.getElementById('result');
+
+
+var height = 10;
+var count = 0;
+
+
+var fillTank = setInterval( function(){
+	
+	if (height == 300) {
+		
+		clearInterval(fillTank);
+		
+		result.style.display = "block";
+		for (var i = 0; i < container.length; i++) {
+			container[i].style.display = "none";
+		}
+
+		var totalClicks = count * 2 / 60;
+		totalClicks = totalClicks.toFixed(2);
+
+		result.innerHTML = "Your click speed is " + totalClicks + " clicks/seconds.";
+
+	}
+
+	time.style.height = height + "px";
+
+	var topMargin = 300 - height;
+	time.style.marginTop = topMargin + "px";
+
+	height = height + 10;
+
+}, 1000 );
+
+counter.addEventListener('click', function(){
+	startMessage.style.display = "none";
+	clickCount.style.display = "block";
+	count++;	
+	clickCount.innerHTML = count;
+});
